fb67e617236863cdf9ca494e2bd03ce939519580
wbob <mail.km@gmx.de>
2016-10-12 15:13:32 +0200

    Commenting Buildscript steps

--- build.sh
+++ build.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
 DOCSET_NAME='Devdocs'
 
-# prior cleanup
+# Prior cleanup
 rm -rf site && rm -rf release
 
-mkdocs build && \
-rm -f site/search_content.json && \
-python3 bin/abs2rel.py -v site && \
-find site -name "*.html" -exec python3 bin/add-dash-anchors.py {} -v \; && \
-mkdir -p release/"$DOCSET_NAME".docset/Contents/Resources/Documents && \
-cp -a site/* release/"$DOCSET_NAME".docset/Contents/Resources/Documents && \
-python3 bin/yaml2sqlite.py -v mkdocs.yml release/"$DOCSET_NAME".docset/Contents/Resources/docSet.dsidx && \
+mkdocs build
+rm -f site/search_content.json
 
-# add icons
+# Recursively converts absolute links to relative links and protocol-relative links to https links
+python3 bin/abs2rel.py -v site
+
+# Add Dash docs anchor tags to html source
+find site -name "*.html" -exec python3 bin/add-dash-anchors.py {} -v \;
+
+# Package folder into Docset structure
+mkdir -p release/"$DOCSET_NAME".docset/Contents/Resources/Documents
+cp -a site/* release/"$DOCSET_NAME".docset/Contents/Resources/Documents
+
+# Reads an MkDocs YAML file and generates a Docset SQLite3 index from the contents
+python3 bin/yaml2sqlite.py -v mkdocs.yml release/"$DOCSET_NAME".docset/Contents/Resources/docSet.dsidx
+
+# Add icons
 cp assets/docset/icon@2x.png release/"$DOCSET_NAME".docset/icon@2x.png
 cp assets/docset/icon.png release/"$DOCSET_NAME".docset/icon.png
 cp assets/docset/Info.plist release/"$DOCSET_NAME".docset/Contents/Info.plist
 
-# create tgz package
+# Create tgz package
 tar -cvzf "$DOCSET_NAME".tgz release/"$DOCSET_NAME".docset
 
-# optionally, move docset to Zeal directory and clear prior copy
+# Optionally, copy Docset to Zeal directory and clear prior copy
 rm -rf ~/.local/share/Zeal/Zeal/docsets/"$DOCSET_NAME".docset/
 cp -R release/"$DOCSET_NAME".docset/ ~/.local/share/Zeal/Zeal/docsets/
